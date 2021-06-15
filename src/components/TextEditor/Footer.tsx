10f2ed439ca6a92e05650aeabcbd9b0e0910e5b6
Balasubramanian Bhuvaneswaran <bhuvaneswaran.b@danfoss.com>
2021-06-15 17:12:09 +0530

    privacy note

--- src/components/TextEditor/Footer.tsx
+++ src/components/TextEditor/Footer.tsx
@@ -1,7 +1,11 @@
 export function Footer() {
   return (
     <footer>
-      <p>
+      <p className="privacy-note">
+        Privacy: Your data will be stored in your browser storage. We won's
+        store any data in server side.
+      </p>
+      <p className="text-center">
         © 2021 Developed by{" "}
         <a
           href="https://www.bhuvaneswaran.com"
